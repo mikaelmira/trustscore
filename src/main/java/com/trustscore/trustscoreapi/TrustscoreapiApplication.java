d17f27fef70c73bb2a536cd22cc37f46feb3b00d
mikaelbernardes <mmira.software@gmail.com>
2026-02-20 18:35:44 -0300

    first commit

---
+++ src/main/java/com/trustscore/trustscoreapi/TrustscoreapiApplication.java
@@ -0,0 +1,13 @@
+package com.trustscore.trustscoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustscoreapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustscoreapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trustscore/trustscoreapi/TrustscoreapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustscore.trustscoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustscoreapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
